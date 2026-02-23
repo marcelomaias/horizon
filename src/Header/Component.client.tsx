00262e436153d88fcb6796d66ba64928926b2aab
Maia <marcelomaias@gmail.com>
2026-02-23 18:30:52 -0300

    setup adjustment

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -34,8 +34,7 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
       <div className="py-8 flex justify-between">
         <Link href="/">
           <Logo loading="eager" priority="high" className="invert dark:invert-0" />
-        </Link>{' '}
-        <span>Test</span>
+        </Link>
         <HeaderNav data={data} />
       </div>
     </header>
