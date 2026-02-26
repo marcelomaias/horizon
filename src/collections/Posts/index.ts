5b925ddbef250fa88e8d85e2b2bc014f21d1d9ab
Maia <marcelomaias@gmail.com>
2026-02-26 12:52:41 -0300

    Models adjustments

--- src/collections/Posts/index.ts
+++ src/collections/Posts/index.ts
@@ -46,6 +46,7 @@ export const Posts: CollectionConfig<'posts'> = {
     },
   },
   admin: {
+    hidden: true,
     defaultColumns: ['title', 'slug', 'updatedAt'],
     livePreview: {
       url: ({ data, req }) =>
